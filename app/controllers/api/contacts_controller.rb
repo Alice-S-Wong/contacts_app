2225d6222aec184bcac11be344885d088a3a2d3b
“Alice <alicexenawong@gmail.com>
2019-11-12 18:31:51 -0800

    Fix bug where updating without address causes error.

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -27,6 +27,7 @@ class Api::ContactsController < ApplicationController
   end
 
   def update
+    coordinates = Geocoder.coordinates(params[:address])
     @contact = Contact.find_by(id: params[:id])
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.middle_name = params[:middle_name] || @contact.middle_name
@@ -34,6 +35,10 @@ class Api::ContactsController < ApplicationController
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.bio = params[:bio] || @contact.bio
+    if coordinates 
+      @contact.latitude = coordinates[0] || @contact.latitude
+      @contact.longitude = coordinates[1] || @contact.longitude
+    end
     @contact.save
     render 'show.json.jb'  
   end
